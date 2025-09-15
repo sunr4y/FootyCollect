8d854fdd1600cacdfa5a7d043b1281ed20d4d8e7
sunr4y <109840029+sunr4y@users.noreply.github.com>
2025-09-15 19:25:12 +0200

    fix: migrate all admin classes to Unfold and fix duplicate actions
    - Migrate all admin classes to use unfold.admin.ModelAdmin
    - Fix duplicate 'delete selected clubs' action in ClubAdmin
    - Fix KitAdmin field references (club -> team)
    - Add proper admin configurations for all models
    - Ensure Unfold theme compatibility across all admin interfaces

--- footycollect/collection/admin.py
+++ footycollect/collection/admin.py
@@ -1,12 +1,13 @@
 # Register your models here.
 
 from django.contrib import admin
+from unfold.admin import ModelAdmin
 
 from .models import BaseItem, Color, Jersey, OtherItem, Outerwear, Pants, Photo, Shorts, Size, Tracksuit
 
 
 @admin.register(BaseItem)
-class BaseItemAdmin(admin.ModelAdmin):
+class BaseItemAdmin(ModelAdmin):
     list_display = ["name", "item_type", "user", "brand", "club", "season", "condition", "created_at"]
     list_filter = ["item_type", "brand", "club", "season", "condition", "is_replica", "is_private", "is_draft"]
     search_fields = ["name", "description", "user__username", "brand__name", "club__name"]
@@ -55,7 +56,7 @@ class BaseItemAdmin(admin.ModelAdmin):
 
 
 @admin.register(Jersey)
-class JerseyAdmin(admin.ModelAdmin):
+class JerseyAdmin(ModelAdmin):
     list_display = ["base_item", "size", "kit", "is_fan_version", "is_signed", "player_name", "number"]
     list_filter = ["is_fan_version", "is_signed", "has_nameset", "is_short_sleeve", "size", "kit"]
     search_fields = ["base_item__name", "player_name", "base_item__user__username"]
@@ -76,55 +77,55 @@ class JerseyAdmin(admin.ModelAdmin):
 
 
 @admin.register(Shorts)
-class ShortsAdmin(admin.ModelAdmin):
+class ShortsAdmin(ModelAdmin):
     list_display = ["base_item", "size", "number", "is_fan_version"]
     list_filter = ["is_fan_version", "size"]
     search_fields = ["base_item__name", "base_item__user__username"]
 
 
 @admin.register(Outerwear)
-class OuterwearAdmin(admin.ModelAdmin):
+class OuterwearAdmin(ModelAdmin):
     list_display = ["base_item", "type", "size"]
     list_filter = ["type", "size"]
     search_fields = ["base_item__name", "base_item__user__username"]
 
 
 @admin.register(Tracksuit)
-class TracksuitAdmin(admin.ModelAdmin):
+class TracksuitAdmin(ModelAdmin):
     list_display = ["base_item", "size"]
     list_filter = ["size"]
     search_fields = ["base_item__name", "base_item__user__username"]
 
 
 @admin.register(Pants)
-class PantsAdmin(admin.ModelAdmin):
+class PantsAdmin(ModelAdmin):
     list_display = ["base_item", "size"]
     list_filter = ["size"]
     search_fields = ["base_item__name", "base_item__user__username"]
 
 
 @admin.register(OtherItem)
-class OtherItemAdmin(admin.ModelAdmin):
+class OtherItemAdmin(ModelAdmin):
     list_display = ["base_item", "type"]
     list_filter = ["type"]
     search_fields = ["base_item__name", "base_item__user__username"]
 
 
 @admin.register(Photo)
-class PhotoAdmin(admin.ModelAdmin):
+class PhotoAdmin(ModelAdmin):
     list_display = ["content_object", "order", "uploaded_at", "user"]
     list_filter = ["uploaded_at", "user"]
     search_fields = ["caption", "user__username"]
 
 
 @admin.register(Color)
-class ColorAdmin(admin.ModelAdmin):
+class ColorAdmin(ModelAdmin):
     list_display = ["name", "hex_value"]
     search_fields = ["name"]
 
 
 @admin.register(Size)
-class SizeAdmin(admin.ModelAdmin):
+class SizeAdmin(ModelAdmin):
     list_display = ["name", "category"]
     list_filter = ["category"]
     search_fields = ["name"]
--- footycollect/core/admin.py
+++ footycollect/core/admin.py
@@ -1,27 +1,66 @@
 # Register your models here.
 from django.contrib import admin
 from django.utils.html import format_html
+from unfold.admin import ModelAdmin
 
 from .models import Brand, Club, Competition, Kit, Season, TypeK
 
-admin.site.register(Competition)
+
+@admin.register(Competition)
+class CompetitionAdmin(ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
 
 
 @admin.register(Club)
-class ClubAdmin(admin.ModelAdmin):
-    list_display = ("name", "show_logo")
+class ClubAdmin(ModelAdmin):
+    list_display = ("name", "country", "show_logo")
+    list_filter = ("country",)
+    search_fields = ("name", "country")
+    actions = ["delete_clubs"]
+    list_per_page = 25
 
     @admin.display(
         description="Logo",
     )
     def show_logo(self, obj):
-        return format_html(
-            '<img src="{}" style="max-height: 50px; max-width: 50px;" />',
-            obj.logo,
-        )
+        if obj.logo:
+            return format_html(
+                '<img src="{}" style="max-height: 50px; max-width: 50px;" />',
+                obj.logo,
+            )
+        return "No logo"
 
 
-admin.site.register(Brand)
-admin.site.register(Kit)
-admin.site.register(Season)
-admin.site.register(TypeK)
+@admin.register(Brand)
+class BrandAdmin(ModelAdmin):
+    list_display = ("name", "show_logo")
+    search_fields = ("name",)
+
+    @admin.display(description="Logo")
+    def show_logo(self, obj):
+        if obj.logo:
+            return format_html(
+                '<img src="{}" style="max-height: 50px; max-width: 50px;" />',
+                obj.logo,
+            )
+        return "No logo"
+
+
+@admin.register(Kit)
+class KitAdmin(ModelAdmin):
+    list_display = ("name", "team", "season")
+    list_filter = ("team", "season")
+    search_fields = ("name", "team__name", "season__year")
+
+
+@admin.register(Season)
+class SeasonAdmin(ModelAdmin):
+    list_display = ("year", "first_year", "second_year")
+    search_fields = ("year",)
+
+
+@admin.register(TypeK)
+class TypeKAdmin(ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
